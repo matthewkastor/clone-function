c39b682f3c8006d8a6065a5c6cbef545be7067e6
Matthew Kastor <matthewkastor@gmail.com>
2013-12-21 10:44:01 -0500

    throw better error messages

--- src/clone-function.js
+++ src/clone-function.js
@@ -14,11 +14,16 @@ License gpl-3.0 http://www.gnu.org/licenses/gpl-3.0-standalone.html
  *  reference to the native function.
  */
 function cloneFunction(func) {
-    var out;
-    if (/\[native code\]/.test(func.toString())) {
-        out = func;
-    } else {
-        out = eval('(function(){return ' + func.toString() + '}());');
+    var out, str;
+    try {
+        str = func.toString();
+        if (/\[native code\]/.test(str)) {
+            out = func;
+        } else {
+            out = eval('(function(){return ' + str + '}());');
+        }
+    } catch (e) {
+        throw new Error(e.message + '\r\n\r\n' + str);
     }
     return out;
 }
